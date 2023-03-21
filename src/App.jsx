5ad7c757d9fed9092d92dd200b1512ff3ea2273f
Fabian Lopez <fm.lopez.reyes@gmail.com>
2023-03-20 21:25:33 -0300

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from "./components/Navigations/Navbar";
-import Home from './components/Pages/Home';
-import Poke from './components/pages/Poke';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navigations/Navbar";
+import Home from "./components/Pages/Home";
+import Poke from "./components/pages/Poke";
 
 function App() {
-
   return (
     <div className="App">
       <Router>
-        <Navbar/>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/poke' element={<Poke/>}/>
-          </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/poke" element={<Poke />} />
+        </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
 export default App;
